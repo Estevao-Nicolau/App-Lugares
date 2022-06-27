6109588f2aa573c2e2dfa54a7106ac5609524995
nicolau <estevao_nicolau@hotmail.com>
2022-06-27 19:22:39 -0300

    :tada:page-routes

---
+++ lib/page/places_list_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:greatplaces/routes/app_routes.dart';
+
+class PlacesListPage extends StatelessWidget {
+  const PlacesListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meus Lugares'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
