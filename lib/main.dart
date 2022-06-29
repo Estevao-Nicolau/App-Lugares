78942173b5fccd35cfed65d0beb42f8d6d071b3e
nicolau <estevao_nicolau@hotmail.com>
2022-06-29 12:48:04 -0300

    :sparkles:provider

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       ),
       home: PlacesListPage(),
       routes: {
-        AppRoutes.PLACE_FORM: ((context) => PlaceForm())
+        AppRoutes.PLACE_FORM: ((context) => PlaceFormPage())
       },
     );
   }
